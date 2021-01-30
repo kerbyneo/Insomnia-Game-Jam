2749ca5a6962265e712423a4e130de812387d623
Hongke <luohongke2000@gmail.com>
2021-01-31 03:39:54 +0800

    color through wall

---
+++ Assets/wall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    public control player;
+    public BoxCollider2D box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.colored)
+        {
+            box.enabled = false;
+        }
+        else
+        {
+            box.enabled = true;
+        }
+    }
+}
