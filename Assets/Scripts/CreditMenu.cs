c90ca743025f59dbd395df62f80fc9f52732e21b
Kerby <kn1578@nyu.edu>
2021-01-31 23:37:29 +0800

    Combined Everything

---
+++ Assets/Scripts/CreditMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CreditMenu : MonoBehaviour
+{
+    public GameObject creditPanal;
+    
+    public void OpenCredit(){
+        if(creditPanal !=null){
+            creditPanal.SetActive(true);
+        }
+        
+    }
+
+    public void CloseCredit(){
+        creditPanal.SetActive(false);
+        Debug.Log("turn off");
+    }
+}
