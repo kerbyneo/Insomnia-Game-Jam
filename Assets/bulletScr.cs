9bae5c2c53eca5ef07e698e50d98c5d28ac56a75
Hongke <luohongke2000@gmail.com>
2021-01-31 08:07:50 +0800

    made the gun

---
+++ Assets/bulletScr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScr : MonoBehaviour
+{
+    public bool right = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (right)
+        {
+            transform.Translate(0.2f,0,0);
+        }
+        else
+        {
+            transform.Translate(-0.2f,0,0);
+        }
+    }
+}
---
+++ Assets/shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public int counter;
+    public bool right;
+    public int speed = 70;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        counter += 1;
+        if (counter > speed)
+        {
+            GameObject newBullet = Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.Euler(0, 0, 0));
+            bulletScr scr = newBullet.GetComponent<bulletScr>();
+            if (right)
+            {
+                scr.right = true;
+            }
+            else
+            {
+                scr.right = false;
+            }
+            counter = 0;
+        }
+    }
+}
